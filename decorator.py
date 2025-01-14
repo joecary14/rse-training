25f34d991f7067ca64dc4af249e870529e75d119
Joseph Cary <bras4813@ox.ac.uk>
2025-01-14 16:19:43 +0000

    mergeconfluct

--- decorator.py
+++ decorator.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-def my_decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
-    return wrapper
-
-@my_decorator
-def say_hello():
-    print("Hello!")
-    
-@my_decorator
-def say_goodbye():
-    print("Goodbye!")
-    
-say_goodbye()
-say_hello()
-=======
 import time
 
 def time_calculation_decorator(func):
@@ -37,4 +18,3 @@ def measure_me(n):
     return total
 
 measure_me(1000000)
->>>>>>> 8b004ef (Practice exercises)
