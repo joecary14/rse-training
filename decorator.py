93cea37f04b0b32445d56a498734ed74749c13b5
Joseph Cary <bras4813@ox.ac.uk>
2025-01-14 16:17:44 +0000

    Practice exercises

--- decorator.py
+++ decorator.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def my_decorator(func):
     def wrapper():
         print("Something is happening before the function is called.")
@@ -15,3 +16,25 @@ def say_goodbye():
     
 say_goodbye()
 say_hello()
+=======
+import time
+
+def time_calculation_decorator(func):
+    def wrapper(*args):
+        start_time = time.process_time_ns()
+        func(*args)
+        end_time = time.process_time_ns()
+        print(f"Time taken to execute the function: {end_time - start_time} ns")
+        print(f"stime: {start_time}, etime: {end_time}")
+    return wrapper
+
+@time_calculation_decorator
+def measure_me(n):
+    total = 0
+    for i in range(n):
+        total += i * i
+
+    return total
+
+measure_me(1000000)
+>>>>>>> 8b004ef (Practice exercises)
